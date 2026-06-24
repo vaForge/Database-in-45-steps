357f290e0f7872f744e52a85d4c37b3fc0754b46
Varshith <kotipallianjan@gmail.com>
2026-06-24 20:39:27 +0530

    0202 done

--- db_project/0202/row.go
+++ db_project/0202/row.go
@@ -1,5 +1,10 @@
 package db0202
 
+import (
+	"errors"
+	"slices"
+)
+
 type Schema struct {
 	Table string
 	Cols  []Column
@@ -17,12 +22,71 @@ func (schema *Schema) NewRow() Row {
 	return make(Row, len(schema.Cols))
 }
 
-func (row Row) EncodeKey(schema *Schema) (key []byte)
+func (row Row) EncodeKey(schema *Schema) (key []byte) {
+	key = append([]byte(schema.Table), 0x00)
+	check(len(row) == len(schema.Cols))
+	for idx, value := range row {
+		check(value.Type == schema.Cols[idx].Type)
+		if slices.Contains(schema.PKey, idx) {
+			key = row[idx].Encode(key)
+		}
+	}
+	return key
+}
 
-func (row Row) EncodeVal(schema *Schema) (val []byte)
+func (row Row) EncodeVal(schema *Schema) (val []byte) {
+	check(len(row) == len(schema.Cols))
+	for idx, value := range row {
+		check(value.Type == schema.Cols[idx].Type)
+		if !slices.Contains(schema.PKey, idx) {
+			val = row[idx].Encode(val)
+		}
+	}
+	return val
+}
 
-func (row Row) DecodeKey(schema *Schema, key []byte) (err error)
+func (row Row) DecodeKey(schema *Schema, key []byte) (err error) {
+	check(len(row) == len(schema.Cols))
+	if len(key) < len(schema.Table)+1 {
+		return errors.New("bad key")
+	}
+	if string(key[:len(schema.Table)+1]) != schema.Table+"\x00" {
+		return errors.New("bad key")
+	}
+	key = key[len(schema.Table)+1:]
 
-func (row Row) DecodeVal(schema *Schema, val []byte) (err error)
+	for idx, col := range schema.Cols {
+		if !slices.Contains(schema.PKey, idx) {
+			continue
+		}
+		row[idx] = Cell{Type: col.Type}
+		if key, err = row[idx].Decode(key); err != nil {
+			return err
+		}
+	}
+	if len(key) != 0 {
+		return errors.New("trailing garbage")
+	}
+	return nil
+}
+
+func (row Row) DecodeVal(schema *Schema, val []byte) (err error) {
+	check(len(row) == len(schema.Cols))
+
+	for idx, col := range schema.Cols {
+		if slices.Contains(schema.PKey, idx) {
+			continue
+		}
+		row[idx] = Cell{Type: col.Type}
+		if val, err = row[idx].Decode(val); err != nil {
+			return err
+		}
+	}
+
+	if len(val) != 0 {
+		return errors.New("trailing garbage")
+	}
+	return nil
+}
 
 // QzBQWVJJOUhU https://trialofcode.org/
