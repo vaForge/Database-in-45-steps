820937f59059cfadcb1cd1dcf2daf6df9448796a
Varshith <kotipallianjan@gmail.com>
2026-06-24 17:58:37 +0530

    0105 done

--- db_project/0105/log.go
+++ db_project/0105/log.go
@@ -1,6 +1,7 @@
 package db0105
 
 import (
+	"io"
 	"os"
 )
 
@@ -25,6 +26,15 @@ func (log *Log) Write(ent *Entry) error {
 	return log.fp.Sync() // fsync
 }
 
-func (log *Log) Read(ent *Entry) (eof bool, err error)
+func (log *Log) Read(ent *Entry) (eof bool, err error) {
+	err = ent.Decode(log.fp)
+	if err == io.EOF || err == io.ErrUnexpectedEOF || err == ErrBadSum {
+		return true, nil
+	} else if err != nil {
+		return false, err
+	} else {
+		return false, nil
+	}
+}
 
 // QzBQWVJJOUhU https://trialofcode.org/
